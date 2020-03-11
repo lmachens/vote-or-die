53693443a53edae1c647a84bd6e4355b3245ac0e
lmachens <leon.machens@googlemail.com>
2020-03-11 14:29:21 +0100

    Add header component with logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div>vote or die</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
