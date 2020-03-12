bfac387b122a04211bf570a83b666d6597eb4683
Leon Machens <leon.machens@googlemail.com>
2020-03-12 10:20:32 +0100

    Add prettier configuration and prettify files (#8)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Add from "./pages/Add";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Vote from "./pages/Vote";
-import Result from "./pages/Result";
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Add from './pages/Add';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Vote from './pages/Vote';
+import Result from './pages/Result';
 
 function App() {
   return (
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Card from "../components/Card";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from '../components/Card';
 
 function Result() {
   return (
