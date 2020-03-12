1ad3fda65e2f0ceb14e4ada0912a7022fe15b14e
lmachens <leon.machens@googlemail.com>
2020-03-12 11:56:36 +0100

    Add form states

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -4,28 +4,62 @@ import './Add.css';
 import Button from '../components/Button';
 
 function Add() {
+  const [question, setQuestion] = React.useState('');
+  const [answerOne, setAnswerOne] = React.useState('');
+  const [answerTwo, setAnswerTwo] = React.useState('');
+  const [answerThree, setAnswerThree] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const poll = {
+      question: question,
+      answerOne: answerOne,
+      answerTwo: answerTwo,
+      answerThree: answerThree
+    };
+
+    alert(JSON.stringify(poll));
+  }
+
   return (
     <Card>
-      <form className="add-form">
+      <form className="add-form" onSubmit={handleSubmit}>
         <input
           className="add-form__input add-form__input-question"
           type="text"
           placeholder="Enter question"
+          value={question}
+          onChange={event => {
+            setQuestion(event.target.value);
+          }}
         />
         <input
           className="add-form__input add-form__input-answer"
           type="text"
           placeholder="First answer"
+          value={answerOne}
+          onChange={event => {
+            setAnswerOne(event.target.value);
+          }}
         />
         <input
           className="add-form__input add-form__input-answer"
           type="text"
           placeholder="Second answer"
+          value={answerTwo}
+          onChange={event => {
+            setAnswerTwo(event.target.value);
+          }}
         />
         <input
           className="add-form__input add-form__input-answer"
           type="text"
           placeholder="Third answer"
+          value={answerThree}
+          onChange={event => {
+            setAnswerThree(event.target.value);
+          }}
         />
         <Button>Create Poll</Button>
       </form>
