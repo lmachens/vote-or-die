0431ca9f1f452caa9cca481eb3c8087e77560945
lmachens <leon.machens@googlemail.com>
2020-03-12 11:02:57 +0100

    Style form and inputs

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Card from '../components/Card';
+import './Add.css';
+import Button from '../components/Button';
 
 function Add() {
   return (
     <Card>
-      <form>
-        <input type="text" placeholder="Enter question" />
-        <input type="text" placeholder="First answer" />
-        <input type="text" placeholder="Second answer" />
-        <input type="text" placeholder="Third answer" />
-        <input type="submit" value="Create Poll" />
+      <form className="add-form">
+        <input
+          className="add-form__input add-form__input-question"
+          type="text"
+          placeholder="Enter question"
+        />
+        <input
+          className="add-form__input add-form__input-answer"
+          type="text"
+          placeholder="First answer"
+        />
+        <input
+          className="add-form__input add-form__input-answer"
+          type="text"
+          placeholder="Second answer"
+        />
+        <input
+          className="add-form__input add-form__input-answer"
+          type="text"
+          placeholder="Third answer"
+        />
+        <Button>Create Poll</Button>
       </form>
     </Card>
   );
